f3d8083e8e6b8f27cae6f60b4d8bdc46bcca2f39
Владимир Шефер <shefervova999@gmail.com>
2021-05-01 23:37:05 +0700

    с сипипишником падлла немного разобрался

--- superultrauniquegame/paddle.cpp
+++ superultrauniquegame/paddle.cpp
@@ -1,1 +1,27 @@
 #include "paddle.h"
+#include <iostream>
+
+PaddleClass::PaddleClass(int posX, int posY)
+{
+    originalX = posX;
+    originalY = posY;
+    x = posX;
+    y = posY;
+}
+
+void PaddleClass::Reset()
+{
+    x = originalX;
+    y = originalY;
+}
+
+int PaddleClass::getX() { return x; }
+int PaddleClass::GetY() { return y; }
+
+void PaddleClass::MoveUP() { y--; }
+void PaddleClass::MoveDOWN() { y++; }
+
+void PaddleClass::PrintPaddleCoordinates()
+{
+    cout << "Paddle [" << x << "," << y << "]" << endl;
+}
